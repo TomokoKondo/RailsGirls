223b3289b3ffa6ac7ed5e0760a031f24a1c9df0f
satomicchy <satomi_t_sora@yahoo.co.jp>
2015-12-27 17:27:25 +0900

    debug navbar active methods

--- projects/railsgirls/app/helpers/application_helper.rb
+++ projects/railsgirls/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def active_root
+    if current_page?("/") || current_page?("/ideas")
+      "active"
+    end
+  end
+
+  def active_new_idea
+    if current_page?("/ideas/new")
+      "active"
+    end
+  end
 end
